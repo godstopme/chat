50ff5a48866c26effcae7082cab776cde97e4d89
Nica <33498296+godstopme@users.noreply.github.com>
2018-04-07 22:11:17 +0300

    signup view & template

--- app/wschat/accounts/urls.py
+++ app/wschat/accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from django.contrib.auth import views
 
+from .views import SignUpView
+
 urlpatterns = [
+    path('signup/', SignUpView.as_view(), name='signup'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
 ]
--- app/wschat/accounts/views.py
+++ app/wschat/accounts/views.py
@@ -1,2 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponseRedirect
+from django.views.generic import CreateView
 
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'registration/signup.html'
+    success_url = '/'  # change to chat
+
+    def form_valid(self, form):
+        self.object = form.save()
+
+        data = form.cleaned_data
+
+        username = data.get('username')
+        password = data.get('password1')
+
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
+
+        return HttpResponseRedirect(self.get_success_url())
