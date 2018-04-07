e598271096ac0343ab74c8b8999bafc2e74e8dca
Nica <33498296+godstopme@users.noreply.github.com>
2018-04-07 20:53:45 +0300

    add login view & template

--- app/wschat/urls.py
+++ app/wschat/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('logout/', views.logout, name='logout'),
+    path('login/', views.login, name='login'),
 ]
