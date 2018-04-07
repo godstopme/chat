6d5b59f228afdf8b9b1ab868f8a6686a09134687
Nica <33498296+godstopme@users.noreply.github.com>
2018-04-08 01:42:23 +0300

    implement messaging via websockets

--- app/wschat/chat/consumers.py
+++ app/wschat/chat/consumers.py
@@ -1,17 +1,29 @@
 import json
+from datetime import datetime
+from channels.generic.websocket import AsyncWebsocketConsumer
 
-from channels.generic.websocket import WebsocketConsumer
 
+class ChatConsumer(AsyncWebsocketConsumer):
+    group_name = 'channels_group'
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
+    async def connect(self):
+        await self.channel_layer.group_add(self.group_name, self.channel_name)
+        await self.accept()
 
-    def disconnect(self, code):
-        pass
+    async def disconnect(self, code):
+        await self.channel_layer.group_discard(self.group_name, self.channel_name)
 
-    def receive(self, text_data=None, bytes_data=None):
-        text_data_json = json.loads(text_data)
-        text_data = json.dumps({'message': text_data_json['message']})
+    async def receive(self, text_data=None, bytes_data=None):
+        payload = {
+            'type': 'send_message',
+            'username': self.scope['user'].username,
+            'message': json.loads(text_data)['message'],
+            'datetime': datetime.now().isoformat(),
+        }
 
-        self.send(text_data=text_data)
+        await self.channel_layer.group_send(self.group_name, payload)
+
+    async def send_message(self, event):
+        event.pop('type', None)
+
+        await self.send(text_data=json.dumps(event))
