0b11426a5bdb6e63432c977de820e5ca0a2db978
Nica <33498296+godstopme@users.noreply.github.com>
2018-04-23 21:48:42 +0300

    change api root urls

--- backend/urls.py
+++ backend/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
+    path('api/accounts/', include('accounts.urls')),
     path('', include('chat.urls'), name='home'),
 ]
